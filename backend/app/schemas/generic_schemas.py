d31c1072eedaed5c108563cb86384c6b5497d942
Bit-Watcher <shaunmarx05@gmail.com>
2026-05-11 17:17:14 +0200

    Fixed errors, finished setup added schemas and implemented login/registartion/confirm user endpoints

---
+++ backend/app/schemas/generic_schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from typing import Any, Optional
+
+class APIResponse(BaseModel):
+    status: str = "success"
+    message: Optional[str] = None
+    data: Optional[Any] = None
+
+class ErrorResponse(BaseModel):
+    status: str = "error"
+    detail: str
