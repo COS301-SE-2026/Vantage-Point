16986d1b85ab91f82b19ad5192a746b54601fed0
Vele.Ndamulelo <velendamulelo057@gmail.com>
2026-05-14 12:51:46 +0200

    feat(UI): added UI implementation from figma designs

---
+++ frontend/src/landing/app/components/RegisterPage.tsx
@@ -0,0 +1,9 @@
+import RegisterComponent from "../../imports/Register/Register";
+
+export default function RegisterPage() {
+  return (
+    <div className="w-screen h-screen bg-white overflow-hidden">
+      <RegisterComponent />
+    </div>
+  );
+}
