23d3d585e73ed0f6065a553120b81ed0fd9ad3fd
Vele.Ndamulelo <velendamulelo057@gmail.com>
2026-05-19 14:42:39 +0200

    Add profile featured-game asset exports from Figma.

---
+++ frontend/src/landing/app/assets/profile/index.ts
@@ -0,0 +1,5 @@
+/** Profile featured-game assets exported from Figma (vantage-point file). */
+export { default as leagueWildRiftCover } from "./league-wild-rift-cover.png";
+export { default as leagueWildRiftCard } from "./league-wild-rift-card.png";
+export { default as iconLightningBolt } from "./icon-lightning-bolt.png";
+export { default as iconTimeMachine } from "./icon-time-machine.png";
