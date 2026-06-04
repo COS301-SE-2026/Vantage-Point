7853949749d434398c437d15e94aab6b0de82976
OPhelia2024 <greyling.ophelia@tuks.co.za>
2026-06-04 11:11:46 +0000

    Added code to connect puuid to participant id in data

--- backend/app/pred_engine/Data_Converter/src/data_processing.py
+++ backend/app/pred_engine/Data_Converter/src/data_processing.py
@@ -1,4 +1,10 @@
-import MatchObj
+import backend.app.pred_engine.Data_Converter.src.MatchInfoObjects as MatchInfoObjects
+from MatchInfoObjects import MatchTimeObj
+from MatchInfoObjects import MatchDataObj
+
+#Get data from api
+tempMatch = MatchDataObj
+tempTimeline = MatchTimeObj
 
 def team_data_extract(puuid):
     #extract data from team that has puuid
@@ -7,7 +13,13 @@ def team_data_extract(puuid):
 
 def player_data_extract(puuid):
     #extract data of specific player according to puuid
-    print()
+
+    #get participant id that matches puuid
+    for i in MatchTimeObj.info.participants:
+        if i.puuid == puuid:
+            partID = i.partcipantId
+    
+    #pull out all data from MatchObj file that is relevant to player
 
 def map_data_extract(puuid):
     #extract data fro map overlay
