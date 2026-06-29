7e5c8e7f271847431028687330cf5db82e82bfff
Life220 <fberrino.pro@gmail.com>
2026-06-29 17:49:19 +0200

    Fix import issue after moving DashboardShell to components due to conflict

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -5,7 +5,7 @@ import {
   useNavigate,
   useSearchParams,
 } from "react-router";
-import DashboardShell, { type DashboardSection } from "./DashboardShell";
+import DashboardShell, { type DashboardSection } from "../components/DashboardShell";
 import type { DashboardOutletContext } from "../context/dashboardLayoutContext";
 import { fetchPlayerProfile } from "../api/profile";
 import { useAuth } from "../context/AuthContext";
