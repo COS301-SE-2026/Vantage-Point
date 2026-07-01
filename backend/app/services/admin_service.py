33cfbe5ce53aff469b3495781f02b52d00f82547
Bit-Watcher <shaunmarx05@gmail.com>
2026-07-01 17:02:11 +0200

    Added async calls to each admin call.

--- backend/app/services/admin_service.py
+++ backend/app/services/admin_service.py
@@ -2,6 +2,7 @@ import boto3
 from botocore.exceptions import ClientError
 from app.config import get_settings
 from fastapi import HTTPException
+import asyncio
 
 settings = get_settings()
 
@@ -12,7 +13,8 @@ client = boto3.client("cognito-idp", region_name=settings.aws_region) #type: ign
 class admin_service:
     async def get_users(self, limit: int = 10):
        try: 
-            response = client.list_users(
+            response = await asyncio.to_thread(
+            client.list_users,
             UserPoolId=settings.cognito_user_pool_id,
             Limit=limit
             )
@@ -29,7 +31,8 @@ class admin_service:
 
     async def get_user(self, username: str):
         try:
-            response = client.admin_get_user(
+            response = await asyncio.to_thread(
+                client.admin_get_user,
                 UserPoolId=settings.cognito_user_pool_id,
                 Username=username
             )
@@ -47,7 +50,8 @@ class admin_service:
 
     async def add_user_to_group(self, username:str, group: str="Users"):
         try:
-            response = client.admin_add_user_to_group(
+            response = await asyncio.to_thread(
+                client.admin_add_user_to_group,
                 UserPoolId=settings.cognito_user_pool_id,
                 Username=username,
                 GroupName=group
@@ -66,7 +70,8 @@ class admin_service:
     
     async def remove_user_from_group(self, username: str, group: str ="Users"):
         try:
-            response = client.admin_remove_user_from_group(
+            response = await asyncio.to_thread(
+                client.admin_remove_user_from_group,
                 UserPoolId=settings.cognito_user_pool_id,
                 Username=username,
                 GroupName=group
@@ -80,7 +85,8 @@ class admin_service:
     
     async def disable_user(self, username: str):
         try:
-            response = client.admin_disable_user(
+            response = await asyncio.to_thread(
+                client.admin_disable_user,
                 UserPoolId=settings.cognito_user_pool_id,
                 Username=username
             )
@@ -93,7 +99,8 @@ class admin_service:
     
     async def enable_user(self, username: str):
         try:
-            response = client.admin_enable_user(
+            response = await asyncio.to_thread(
+                client.admin_enable_user,
                 UserPoolId=settings.cognito_user_pool_id,
                 Username=username
             )
@@ -106,7 +113,8 @@ class admin_service:
     
     async def set_password(self, username: str, password: str):
         try:
-            response = client.admin_set_user_password(
+            response = await asyncio.to_thread(
+                client.admin_set_user_password,
                 UserPoolId=settings.cognito_user_pool_id,
                 Username=username,
                 Password=password,
@@ -127,7 +135,8 @@ class admin_service:
 
     async def user_global_sign_out(self, username: str):
         try:
-            response = client.admin_user_global_sign_out(
+            response = await asyncio.to_thread(
+                client.admin_user_global_sign_out,
                 UserPoolId=settings.cognito_user_pool_id,
                 Username=username
             )
@@ -140,7 +149,8 @@ class admin_service:
     
     async def delete_user(self, username: str):
         try:
-            response = client.admin_delete_user(
+            response = await asyncio.to_thread(
+                client.admin_delete_user,
                 UserPoolId=settings.cognito_user_pool_id,
                 Username=username
             )
@@ -154,7 +164,8 @@ class admin_service:
 
     async def create_user(self, username: str, email: str, temp_pass: str="TemPass@123"):
         try:
-            response = client.admin_create_user(
+            response = await asyncio.to_thread(
+                client.admin_create_user,
                 UserPoolId=settings.cognito_user_pool_id,
                 Username=username,
                 UserAttributes=[
@@ -176,18 +187,11 @@ class admin_service:
             if error_code == "InvalidParamaterException":
                 raise HTTPException(status_code=422, detail="Invalid username")
             raise HTTPException(status_code=400, detail=error_code)
-            
-
-#     {
-#   "User": {
-#     "Username": "john_doe",
-#     "UserStatus": "FORCE_CHANGE_PASSWORD"
-#   }
-# }
-
+         
     async def create_group(self, group_name: str, precedence: int, description: str):
         try:
-            response = client.create_group(
+            response = await asyncio.to_thread(
+                client.create_group,
                 GroupName=group_name,
                 UserPoolId=settings.cognito_user_pool_id,
                 Description=description,
@@ -202,14 +206,11 @@ class admin_service:
                raise HTTPException(status_code=400, detail="Group name already exist.")
             raise HTTPException(status_code=400, detail=error_code)
         # {
-#   "Group": {
-#     "GroupName": "Admin"
-#   }
-# }
 
     async def update_group_attr(self, group_name: str, precedence: int, description: str):
         try:
-            client.update_group(
+            await asyncio.to_thread(
+                client.update_group,
                 GroupName=group_name,
                 UserPoolId=settings.cognito_user_pool_id,
                 Description=description,
@@ -223,7 +224,8 @@ class admin_service:
 
     async def delete_group(self, group_name: str):
         try:
-            client.delete_group(
+            await asyncio.to_thread(
+                client.delete_group,
                 GroupName=group_name,
                 UserPoolId=settings.cognito_user_pool_id
             )
