4b6074e9c75fa4cf546dee1afed24a2c800b1ac1
Bit-Watcher <shaunmarx05@gmail.com>
2026-07-06 18:23:46 +0200

    Created admin users pydantic model

--- backend/app/Models/admin_model.py
+++ backend/app/Models/admin_model.py
@@ -1,3 +1,10 @@
 from pydantic import BaseModel
 
 class Users(BaseModel):
+    username: str
+    email: str
+    sub: str
+    user_created_date: str
+    user_last_modified_date: str
+    enanled: str
+    user_status: str
