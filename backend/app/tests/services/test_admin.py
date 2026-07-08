06992ec3de5f60749898fe75dbb7c5b473db612f
Bit-Watcher <shaunmarx05@gmail.com>
2026-07-08 16:17:16 +0200

    Implemented get_users success endpoint unit test

--- backend/app/tests/services/test_admin.py
+++ backend/app/tests/services/test_admin.py
@@ -29,11 +29,11 @@ class admin_test_get:
             "UserAttributes": [
                 {
                     "Name": "email",
-                    "Value": "shaunmarx05@gmail.com"
+                    "Value": "shaun@gmail.com"
                 },
                 {
                     "Name": "sub",
-                    "Value": "005cf93c-6031-70fe-58ea-11c03431ba8d"
+                    "Value": "12345"
                 }
             ],
             "UserCreateDate": datetime(
@@ -51,8 +51,8 @@ class admin_test_get:
         response = await admin_service.get_user("shaun")
         
         assert response.username == "shaun"
-        assert response.email == "shaunmarx05@gmail.com"
-        assert response.sub == "005cf93c-6031-70fe-58ea-11c03431ba8d"
+        assert response.email == "shaun@gmail.com"
+        assert response.sub == "12345"
         assert response.user_created_date == datetime.fromisoformat("2026-07-01T20:08:52.115000+02:00")
         assert response.user_last_modified_date == datetime.fromisoformat("2026-07-01T20:09:42.094000+02:00")
         assert response.enabled == True
@@ -119,3 +119,50 @@ class admin_test_get:
 
         assert exec.value.status_code == 400
         assert exec.value.detail == "InternalErrorException"
+
+    #get users unit test
+
+    @staticmethod
+    async def get_users_success(mock_admin_get_users: MagicMock):
+        mock_admin_get_users.return_value = {
+            "Users": [
+                {
+                    "Username": "shaun",
+                    "Attributes": [
+                        {"Name": "email", "Value": "shaun@gmail.com"},
+                        {"Name": "sub", "Value": "12345"},
+                    ],
+                    "UserCreateDate": datetime.now(timezone.utc),
+                    "UserLastModifiedDate": datetime.now(timezone.utc),
+                    "Enabled": True,
+                    "UserStatus": "CONFIRMED",
+                },
+                {
+                    "Username": "john",
+                    "Attributes": [
+                        {"Name": "email", "Value": "john@gmail.com"},
+                        {"Name": "sub", "Value": "67890"},
+                    ],
+                    "UserCreateDate": datetime.now(timezone.utc),
+                    "UserLastModifiedDate": datetime.now(timezone.utc),
+                    "Enabled": False,
+                    "UserStatus": "FORCE_CHANGE_PASSWORD",
+                },
+            ]
+        }
+
+        users = await admin_service.get_users()
+
+        assert len(users) == 2
+
+        assert users[0].username == "shaun"
+        assert users[0].email == "shaun@gmail.com"
+        assert users[0].sub == "12345"
+        assert users[0].enabled is True
+        assert users[0].user_status == "CONFIRMED"
+
+        assert users[1].username == "john"
+        assert users[1].email == "john@gmail.com"
+        assert users[1].sub == "67890"
+        assert users[1].enabled is False
+        assert users[1].user_status == "FORCE_CHANGE_PASSWORD"
