4169dda7e864f086431eec65c9622131731c3e71
OPhelia2024 <greyling.ophelia@tuks.co.za>
2026-07-08 17:56:15 +0000

    Removed the function that sonarqube couldnt understand

--- backend/app/pred_engine/rf_model.py
+++ backend/app/pred_engine/rf_model.py
@@ -3,11 +3,7 @@ from sklearn.metrics import accuracy_score  # type: ignore
 from sklearn.model_selection import GridSearchCV  # type: ignore
 from sklearn.multioutput import MultiOutputClassifier  # type: ignore
 import Data_Converter.src.Converter_Main as converter  # type: ignore
-import pandas as pd
-import numpy as np
 import warnings
-import time
-import csv
 
 warnings.filterwarnings("ignore")
 file_error_text = "Training file not found"
@@ -45,8 +41,8 @@ def hyperparam_gridsearch(x_train, x_test, y_train, y_test, run_cat):
         rf_multi.fit(x_train, y_train)
         y_pred_grid = rf_multi.predict(x_test)
         y_pred_grid_skill = [row[0] for row in y_pred_grid[0 : len(y_pred_grid)]]
-        y_test_sKill = [row[0] for row in y_test[0 : len(y_test)]]
-        scores = accuracy_score(y_pred_grid_skill, y_test_sKill)
+        y_test_skill = [row[0] for row in y_test[0 : len(y_test)]]
+        scores = accuracy_score(y_pred_grid_skill, y_test_skill)
 
     else:
         model_grid.fit(x_train, y_train)
@@ -54,43 +50,6 @@ def hyperparam_gridsearch(x_train, x_test, y_train, y_test, run_cat):
         scores = accuracy_score(y_pred_grid, y_test)
 
     return scores
-
-
-def gini_importance(rf, file_name, run_cat, y_val):
-    # add file check
-    try:
-        open(file_name, "r")
-    except OSError:
-        print(file_error_text)
-        exit()
-
-    with open(file_name, "r") as f:
-        data = csv.reader(f)
-        feature_names = []
-
-    if run_cat == "skill" | run_cat == "role":
-        for row in data:
-            for i in row:
-                if i != y_val and i != "levelUpType" and i != "lane":
-                    feature_names.append(i)
-            break
-        
-        feature_impts = []
-        for clf in rf.estimators_:
-            feature_impts.append(clf.feature_importances_)
-        importances = np.mean(feature_impts, axis=0)
-    else:
-        for row in data:
-            for i in row:
-                if i != y_val:
-                    feature_names.append(i)
-            break
-        importances = rf.feature_importances_
-
-    feature_imp_df = pd.DataFrame(
-        {"Feature": feature_names, "Gini Importance": importances}
-    ).sort_values("Gini Importance", ascending=False)
-    return feature_imp_df
 
 
 def rf_univariate(x_train, x_test, y_train, y_test):
@@ -109,10 +68,10 @@ def rf_multivariate(x_train, x_test, y_train, y_test):
 
     y_pred_grid = rf_multi.predict(x_test)
 
-    y_pred_grid_Skill = [row[0] for row in y_pred_grid[0 : len(y_pred_grid)]]
-    y_test_SKill = [row[0] for row in y_test[0 : len(y_test)]]
+    y_pred_grid_skill = [row[0] for row in y_pred_grid[0 : len(y_pred_grid)]]
+    y_test_skill = [row[0] for row in y_test[0 : len(y_test)]]
 
-    scores = accuracy_score(y_pred_grid_Skill, y_test_SKill)
+    scores = accuracy_score(y_pred_grid_skill, y_test_skill)
     return scores, rf_multi
 
 
@@ -136,28 +95,25 @@ def test_and_eval(file_name, run_cat, y_val):
 
     match run_cat:
         case "champion":
-            base_ac, rf_model = rf_univariate(x_train, x_test, y_train, y_test)
+            base_ac, _ = rf_univariate(x_train, x_test, y_train, y_test)
         case "item":
-            base_ac, rf_model = rf_univariate(x_train, x_test, y_train, y_test)
+            base_ac, _ = rf_univariate(x_train, x_test, y_train, y_test)
         case "skill":
-            base_ac, rf_model = rf_multivariate(x_train, x_test, y_train, y_test)
+            base_ac, _ = rf_multivariate(x_train, x_test, y_train, y_test)
         case "role":
-            base_ac, rf_model = rf_multivariate(x_train, x_test, y_train, y_test)
+            base_ac, _ = rf_multivariate(x_train, x_test, y_train, y_test)
 
     param_ac = hyperparam_gridsearch(x_train, x_test, y_train, y_test, run_cat)
-    feature_dif = gini_importance(rf_model, file_name, run_cat, y_val)
 
     print("")
     print(f"Base accuracy: {base_ac}")
     print(f"Parameter tuned accuracy: {param_ac}")
-    print(feature_dif)
 
 
 ###### FINAL MODELS #######
 
 
 def final_train(file_name, run_cat):
-    start = time.time()
     # add file check
     try:
         open(file_name, "r")
@@ -202,7 +158,4 @@ def final_train(file_name, run_cat):
             y_test_skill = [row[0] for row in y_test[0 : len(y_test)]]
             base_ac = accuracy_score(y_pred_grid_skill, y_test_skill)
 
-    end = time.time()
-    print()
-    print(f"Final Time: {end - start:.2f} seconds")
     return rf_model, base_ac
