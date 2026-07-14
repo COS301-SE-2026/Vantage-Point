64ca6f9c4d80dc57f6f600bcf922d3da43d4805b
Bit-Watcher <shaunmarx05@gmail.com>
2026-07-14 17:58:53 +0200

    Continue to implement router

--- backend/app/api/router/analytics_router.py
+++ backend/app/api/router/analytics_router.py
@@ -17,9 +17,18 @@ from app.Models.riot_schemas import (
 router = APIRouter()
 
 @router.get(
-    "/analytics/map-replay/{matchId}",
+    "/analytics/map-replay/{match_id}",
     response_model=MapReplay,
     tags=["Analytics"]
 )
-async def map_replay(_: Annotated[User, Depends(require_group(10))], matchId: str):
-    return await LiveAnalyticsService.map_replay(matchId)
+async def map_replay(_: Annotated[User, Depends(require_group(10))], match_id: str):
+    return await LiveAnalyticsService.map_replay(match_id)
+
+@router.get(
+    "/analytics/map_suggest_data/{match_id}",
+    response_model=MapSuggestData,
+    tags=["Analytics"]
+)
+async def map_suggest_data(_: Annotated[User, Depends(require_group(10))], match_id: str):
+    return await LiveAnalyticsService.map_suggest_data(match_id)
+
