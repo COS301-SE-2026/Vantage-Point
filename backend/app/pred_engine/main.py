a7f321c3ba597379d600ac0f26275b87460caa80
Life220 <fberrino.pro@gmail.com>
2026-07-15 16:44:34 +0200

    Black format

--- backend/app/pred_engine/main.py
+++ backend/app/pred_engine/main.py
@@ -5,7 +5,7 @@
 # block get run thru knn to get the next block to move to
 # repeat to create optimal path
 
-#jsut did to get good complicance maybe need to change later
+# jsut did to get good complicance maybe need to change later
 # from . import knn_model
 
 # print(knn_model.run_knn([[0.78182287]]))
