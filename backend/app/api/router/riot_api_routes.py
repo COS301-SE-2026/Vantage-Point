9b4812d827aa5233b0019858a4c22df718a3596a
Bit-Watcher <shaunmarx05@gmail.com>
2026-07-20 15:03:44 +0200

    Continue with implementation of riot_service router

--- backend/app/api/router/riot_api_routes.py
+++ backend/app/api/router/riot_api_routes.py
@@ -1,6 +1,7 @@
 from app.services.riot_service import RiotServiceDep
 from fastapi import APIRouter
 from typing import Any
+from app.services.riot_api import get_puuid_by_riot_id
 
 router = APIRouter()
 
@@ -13,3 +14,10 @@ async def get_match_timeline(match_id: str, riot: RiotServiceDep):
 
 async def get_match_ids(riot: RiotServiceDep, server_region: str, puuid: str, count: int=5):
     return await riot.get_match_ids(server_region, puuid, count)
+
+async def get_puuid_by_riot_id(game_name: str, tag_line: str) -> str | None:
+    return await get_puuid_by_riot_id(game_name, tag_line)
+
+async def get_summoner_data(riot: RiotServiceDep, puuid: str):
+    return await riot.get_summoner_data(puuid)
+
