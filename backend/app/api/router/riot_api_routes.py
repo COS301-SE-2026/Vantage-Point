a2fdddee26cd02df0ebeebd5c4e4c91712d5b60f
Bit-Watcher <shaunmarx05@gmail.com>
2026-07-20 15:08:14 +0200

    Start with riot serivce router setup

--- backend/app/api/router/riot_api_routes.py
+++ backend/app/api/router/riot_api_routes.py
@@ -5,19 +5,22 @@ from app.services.riot_api import get_puuid_by_riot_id
 
 router = APIRouter()
 
-
+@router.get(
+        "/riot/get-match",
+        tags=["riot"],
+)
 async def get_match_detail(match_id: str, riot: RiotServiceDep) -> Any:
     return await riot.get_match_detail(match_id)
 
-async def get_match_timeline(match_id: str, riot: RiotServiceDep):
+async def get_match_timeline(match_id: str, riot: RiotServiceDep) -> Any:
     return await riot.get_match_timeline(match_id)
 
-async def get_match_ids(riot: RiotServiceDep, server_region: str, puuid: str, count: int=5):
+async def get_match_ids(riot: RiotServiceDep, server_region: str, puuid: str, count: int=5) -> Any:
     return await riot.get_match_ids(server_region, puuid, count)
 
 async def get_puuid_by_riot_id(game_name: str, tag_line: str) -> str | None:
     return await get_puuid_by_riot_id(game_name, tag_line)
 
-async def get_summoner_data(riot: RiotServiceDep, puuid: str):
+async def get_summoner_data(riot: RiotServiceDep, puuid: str) -> Any:
     return await riot.get_summoner_data(puuid)
 
