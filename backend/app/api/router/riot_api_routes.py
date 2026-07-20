ed7af2e654e2a1e89bcae49281861ff5545414b2
Bit-Watcher <shaunmarx05@gmail.com>
2026-07-20 13:13:57 +0200

    Continue with riotservice

--- backend/app/api/router/riot_api_routes.py
+++ backend/app/api/router/riot_api_routes.py
@@ -8,4 +8,8 @@ router = APIRouter()
 async def get_match_detail(match_id: str, riot: RiotServiceDep) -> Any:
     return await riot.get_match_detail(match_id)
 
+async def get_match_timeline(match_id: str, riot: RiotServiceDep):
+    return await riot.get_match_timeline(match_id)
 
+async def get_match_ids(riot: RiotServiceDep, server_region: str, puuid: str, count: int=5):
+    return await riot.get_match_ids(server_region, puuid, count)
