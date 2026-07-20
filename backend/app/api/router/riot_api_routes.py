640e12449ba478ed3cab2185fea353e47bc19052
Bit-Watcher <shaunmarx05@gmail.com>
2026-07-20 15:11:50 +0200

    Continue with router setup

--- backend/app/api/router/riot_api_routes.py
+++ backend/app/api/router/riot_api_routes.py
@@ -7,14 +7,29 @@ router = APIRouter()
 
 @router.get(
         "/riot/get-match",
+        summary="Gets a match detail",
+        description="Takes in match id and then goes and extracts the data of a match from the riot api(matchv5)",
         tags=["riot"],
 )
 async def get_match_detail(match_id: str, riot: RiotServiceDep) -> Any:
     return await riot.get_match_detail(match_id)
 
+@router.get(
+        "/riot/get-timeline",
+        summary="Gets a match timeline",
+        description="Takes in match id and then goes and extracts the data of a match timeline from the riot api(matchv5)",
+        tags=["riot"],
+)
 async def get_match_timeline(match_id: str, riot: RiotServiceDep) -> Any:
     return await riot.get_match_timeline(match_id)
 
+@router.get(
+        "/riot/get-match-ids",
+        summary="Gtes the match ids of a certain player for x amount",
+        description="It takes in player id and amount of games you want. It then goes and look at the server where the player played and extracts the x" \
+        "amount of games specified",
+        tags=["riot"],
+)
 async def get_match_ids(riot: RiotServiceDep, server_region: str, puuid: str, count: int=5) -> Any:
     return await riot.get_match_ids(server_region, puuid, count)
 
