7e0402732fe7893be96969c7d2f4d645b571034d
Bit-Watcher <shaunmarx05@gmail.com>
2026-07-26 19:29:43 +0200

    created mock user and imported deoencancies

--- backend/app/tests/routes/test_admin_routes.py
+++ backend/app/tests/routes/test_admin_routes.py
@@ -3,5 +3,17 @@ from unittest.mock import AsyncMock, MagicMock, patch
 from fastapi.testclient import TestClient
 from app.api.router.admin_routes import router
 from app.services.admin_service import admin_service
+from app.main import app
+from app.Models.auth_model import UserTest
+from typing import Any
+
+app.include_router(router)
 
 
+mock_user = UserTest(
+    sub="test-sub-123",
+    password="Test@123",
+    username="admin_user",
+    email="admin@test.com",
+    groups=["Admin"],
+)
