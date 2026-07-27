5d14f3115dc3e30aeed34e068072282e374ddf44
Bit-Watcher <shaunmarx05@gmail.com>
2026-07-27 17:11:27 +0200

    Updated imports and models being used

--- backend/app/tests/routes/test_admin_routes.py
+++ backend/app/tests/routes/test_admin_routes.py
@@ -1,30 +1,43 @@
 import pytest
-from unittest.mock import AsyncMock, MagicMock, patch
+from datetime import datetime, timezone
+from unittest.mock import patch
 from fastapi.testclient import TestClient
 from app.api.router.admin_routes import router
 from app.services.admin_service import admin_service
 from app.main import app
-from app.Models.auth_model import UserTest
+from app.Models.auth_model import User
+from app.Models.admin_model import UserResponse
 from typing import Any, cast, reveal_type
 import httpx
 
 app.include_router(router)
 
-
-mock_user1 = UserTest(
-    sub="sub1",
-    password="Test@123",
-    username="user1",
-    email="user1@test.com",
-    groups=["Users","Admin"],
+mock_date = datetime(2026,1,1,tzinfo=timezone.utc)
+mock_admin = User(
+    sub="admin-sub",
+    passwrod="Test@123",
+    username="admin",
+    email="admin@test.com"
 )
 
-mock_user2 = UserTest(
+mock_user1 = UserResponse(
+    sub="sub1",
+    username="user1",
+    email="user1@test.com",
+    user_created_date=mock_date,
+    user_last_modified_date=mock_date,
+    enabled=True,
+    user_status="CONFIRMED"
+)
+
+mock_user2 = UserResponse(
     sub="sub2",
-    password="Test@123",
     username="user2",
     email="user2@test.com",
-    groups=["Users", "Admin"],
+    user_created_date=mock_date,
+    user_last_modified_date=mock_date,
+    enabled=True,
+    user_status="CONFIRMED"
 )
 
 @pytest.mark.asyncio
