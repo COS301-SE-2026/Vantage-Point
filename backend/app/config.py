f6caa7b96383be3d59cdb816df54ed5e935064c7
Life220 <fberrino.pro@gmail.com>
2026-07-27 02:29:33 +0200

    Added Database url to config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -12,7 +12,12 @@ class Settings(BaseSettings):
     riot_region: str = "americas"
     riot_platform: str = "na1"
 
-    # ============ AWS Cognito Configuration ============
+    # ============= Database Configuration =============
+    database_url: str = ""
+
+    # ============ AWS Credentials & Cognito Configuration ============
+    aws_access_key_id: str = ""
+    aws_secret_access_key: str = ""
     aws_region: str = "eu-west-1"
     cognito_user_pool_id: str = ""
     cognito_client_id: str = ""
@@ -22,6 +27,7 @@ class Settings(BaseSettings):
     debug: bool = True
     host: str = "0.0.0.0"  # nosec B104
     port: int = 8000
+
     # ============ Rate Limiting ============
     rate_limit_requests: int = 20
     rate_limit_seconds: int = 1
