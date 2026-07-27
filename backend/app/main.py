cb9ace6caf88cc0bb16f82af776424c58900ccd6
Life220 <fberrino.pro@gmail.com>
2026-07-27 02:31:53 +0200

    Added origins for middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -60,9 +60,16 @@ app = FastAPI(
 # app.add_middleware(SlowAPIMiddleware)
 # CORS for frontend
 # 3000 = React default, 5173 = Vite default.
+origins = [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:3000",
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=".*",
+    allow_origins=origins,
+    # allow_origin_regex=".*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
