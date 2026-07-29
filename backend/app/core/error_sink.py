e45a78e2ddcd10ba6d6a691ed2fb7a7bb8e4082f
Bit-Watcher <shaunmarx05@gmail.com>
2026-07-29 17:04:46 +0200

    db_sink_error. saves errors to the db which will leter be called and used in frontend

--- backend/app/core/error_sink.py
+++ backend/app/core/error_sink.py
@@ -3,7 +3,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 from fastapi import HTTPException
 from app.config import get_settings
-
+from typing import Any
+from app.schemas.logs import ErrorLog
 settings = get_settings()
 
 if settings.database_url is None:
@@ -19,6 +20,34 @@ _Default_Codes = {
     "info": "#INFO"
 }
 
+def db_error_sink(message: Any) -> None:
+    record: Any = message.record
+    extra: Any = record["extra"]
+    exc_info: Any = record["exception"]
+    severity:str = record["levek"].name.lower()
 
+    error_type = (exc_info.type__name__ if exc_info and exc_info.type else extra.get("error_type", "Event"))
+
+    stack_trace = ("".join(traceback.format_exception(*exc_info))
+                   if exc_info and exc_info.value else None)
+
+    session: Session = SyncSessionLocal()
+    try:
+        error_log: ErrorLog = ErrorLog(
+            error_code=extra.get("error_code", _Default_Codes.get(severity, "#000")),
+            service=extra.get("service", record["name"])
+            endpoint=extra.get("endpoint"),
+            error_type=error_type,
+            message=record["message"],
+            stack_trace=stack_trace,
+            severity=severity
+        )
+        session.add(error_log)
+        session.commit()
+    except Exception:
+        session.rollback()
+        print("Failed to upload log to db:", traceback.format_exc())
+    finally:
+        session.close()
 
 
