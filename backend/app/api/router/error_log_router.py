c6aa754cf926c3896d5c12bea31e19603f38677f
Bit-Watcher <shaunmarx05@gmail.com>
2026-07-29 22:17:29 +0200

    Implement get error router, get more detail on it

--- backend/app/api/router/error_log_router.py
+++ backend/app/api/router/error_log_router.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.Models.auth_model import User
 from app.api.auth import require_group
 from app.Models.error_log_models import GetErrorLogLists, ToggleReview
+from app.database.models import ErrorLog
 from app.database.session import get_session
 
 router = APIRouter()
@@ -17,4 +18,10 @@ async def list_errors(_current_user: Annotated[User, Depends(require_group(20))]
         limit: int = 30,
         offset: int = 10)-> Any:
     response: list[GetErrorLogLists] = await ErrorClass.get_errors_for_dashboard(session, severity, service, reviewed, limit, offset)
+    return response
+
+@router.get("/dashboard/errors/{error_id}", tags=["dashboard"])
+async def list_error(_current_user: Annotated[User, Depends(require_group(20))], session: Annotated[AsyncSession, Depends(get_session)],
+        error_id: int)-> Any:
+    response: ErrorLog = await ErrorClass.get_error_log(session, error_id)
     return response
