556a3e47aef1cf2176a0b4cdb362aeced5fe507d
Bit-Watcher <shaunmarx05@gmail.com>
2026-07-30 18:34:29 +0200

    Added new week

--- backend/app/services/dashboard_services.py
+++ backend/app/services/dashboard_services.py
@@ -1,25 +1,34 @@
 from app.services.admin_service import admin_service
 from typing import Any
 from app.Models.admin_model import UserResponse
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 
 class DashboardService:
 
-    users: list[UserResponse]=[]
+    users: list[UserResponse] | None=None
 
     @staticmethod
     async def new_users_today() -> int:
-        users=await admin_service.get_users()
+        DashboardService.users=await admin_service.get_users()
         today = datetime.now(timezone.utc).date
         new_today = sum(
-            1 for user in users
-            if user.user_created_date ==today()
+            1 for user in DashboardService.users
+            if user.user_created_date == today
         )
         return new_today
 
     @staticmethod
-    async def new_users_this_week():
+    async def new_users_this_week() -> int:
+        DashboardService.users=await admin_service.get_users()
+
+        week_ago = datetime.now(timezone.utc) - timedelta(days=7)
+        new_week = sum(
+            1 for user in DashboardService.users
+            if user.user_created_date >= week_ago
+        )
+        return new_week
+
 
     @staticmethod
-    async def new_users_this_month():
+    async def new_users_this_month() -> int:
