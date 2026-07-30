75b99bfb2be1dbbf5910de07e61a7d089915defc
Bit-Watcher <shaunmarx05@gmail.com>
2026-07-30 18:30:05 +0200

    New users today

--- backend/app/services/dashboard_services.py
+++ backend/app/services/dashboard_services.py
@@ -1,10 +1,22 @@
 from app.services.admin_service import admin_service
+from typing import Any
+from app.Models.admin_model import UserResponse
+from datetime import datetime, timezone
 
 
 class DashboardService:
 
+    users: list[UserResponse]=[]
+
     @staticmethod
-    async def new_users_today():
+    async def new_users_today() -> int:
+        users=await admin_service.get_users()
+        today = datetime.now(timezone.utc).date
+        new_today = sum(
+            1 for user in users
+            if user.user_created_date ==today()
+        )
+        return new_today
 
     @staticmethod
     async def new_users_this_week():
