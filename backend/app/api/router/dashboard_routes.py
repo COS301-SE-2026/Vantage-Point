f1a9c0036df390153c8bc751e34344e61392daf1
Bit-Watcher <shaunmarx05@gmail.com>
2026-08-10 18:10:33 +0200

    Finished with dashboard service router

--- backend/app/api/router/dashboard_routes.py
+++ backend/app/api/router/dashboard_routes.py
@@ -2,10 +2,9 @@ from app.services.dashboard_services import DashboardService
 from fastapi import Depends, APIRouter
 from app.Models.auth_model import User
 from app.api.auth import require_group
-from typing import Any, Annotated
+from typing import Annotated
 from app.database.session import get_session
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.Models.admin_model import UserResponse
 
 router = APIRouter()
 
@@ -57,7 +56,7 @@ async def get_unconfirmed_users(_: Annotated[User, Depends(require_group(20))]):
 @router.get(
     "/dashboard/weekly-growth",
     response_model=int,
-    summary="Get unconfirmed users from cognito pool",
+    summary="Get the weekly growth of new users registered",
     tags=["dashboard"],
 )
 async def get_weekly_growth(_: Annotated[User, Depends(require_group(20))]):
@@ -66,9 +65,27 @@ async def get_weekly_growth(_: Annotated[User, Depends(require_group(20))]):
 @router.get(
     "/dashboard/weekly-growth",
     response_model=int,
-    summary="Get unconfirmed users from cognito pool",
+    summary="Get the monthly growth of new users registered",
     tags=["dashboard"],
 )
 async def get_monthly_growth(_: Annotated[User, Depends(require_group(20))]):
     return await DashboardService.monthly_growth()
 
+@router.get(
+    "/dashboard/total-matches",
+    response_model=int,
+    summary="Get total matches from matches table in database",
+    tags=["dashboard"],
+)
+async def get_total_matches(_: Annotated[User, Depends(require_group(20))], session: Annotated[AsyncSession, Depends(get_session)]):
+    return await DashboardService.get_total_matches(session)
+
+
+@router.get(
+    "/dashboard/storage_used",
+    response_model=int,
+    summary="Get storage used in aws cognito",
+    tags=["dashboard"],
+)
+async def get_s3_storage(_: Annotated[User, Depends(require_group(20))]):
+    return await DashboardService.get_s3_storage_used()
