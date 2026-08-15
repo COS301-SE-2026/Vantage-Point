0390d3912be97926fae5c7ef0be5846d2fcbd5c1
OPhelia2024 <greyling.ophelia@tuks.co.za>
2026-08-15 07:40:35 +0000

    All AI output functions done

--- backend/app/pred_engine/ai_caller.py
+++ backend/app/pred_engine/ai_caller.py
@@ -1,14 +1,30 @@
 import json
 from app.pred_engine import AI_models as ai  # type: ignore
 
-#contains all funtions for frontend to access
+#make models a global thing for this file or store in db
+champ_rf = None
+item_rf = None 
+role_rf = None 
+skill_rf = None 
+knn_model = None
 
-def get_skill_pred(rf_model, data):
+#contains all funtions for frontend to access
+def create_models():
+    c = ai.create_rf_models()
+    #k = ai.create_knn_model()
+
+    champ_rf = c
+    #item_rf = i
+    #role_rf = r
+    #skill_rf = s
+    #knn_model = k
+
+def get_skill_pred(data):
     #TODO: replace with code to pull from db?
     with open("datasets/champions.json", "r") as file:
         data_file = json.load(file)
 
-    y_output = ai.run_rf(rf_model, data, "skill")
+    y_output = ai.run_rf(skill_rf, data, "skill")
     #y_output is skillslot, levelUpType
 
     skill_id = None
@@ -35,7 +51,12 @@ def get_skill_pred(rf_model, data):
     return skill
     #example output: "Look at upgrading [skill] at this point", "It might be good to level [skill] here"
 
-def get_item_name_icon(item_id):
+def get_item_pred(data):
+    y_output = ai.run_rf(item_rf, data, "item")
+    item_id = None
+    if y_output != None:
+        item_id = y_output
+
     #TODO: replace with code to pull from db?
     with open("datasets/items.json", "r") as file:
         data = json.load(file)
@@ -46,14 +67,52 @@ def get_item_name_icon(item_id):
     return item_name, item_icon
     #name is for AI output, name + icon is for timeline analysis page
     
-def get_role_pred():
-    print()
+def get_role_pred(data):
+    y_output = ai.run_rf(role_rf, data, "role")
+    #gives teampos, lane
 
-def get_champ_pred():
-    print()
+    #add check if champ allows this position?
+    with open("datasets/champions.json", "r") as file:
+        data_file = json.load(file)
+    champId = data[2]
+
+    print(y_output) #for testing purposes
+
+    pos, lane = None, None
+    if y_output != None:
+        pos = y_output[0]
+        lane = y_output[1]
+
+    allowed = False
+    for i in data_file:
+        if i["id"] == champId:
+            positions = i["positions"]
+            if pos in positions:
+                allowed = True
+
+    return pos, lane, allowed
+    
+def get_champ_pred(data):
+    #TODO: replace with code to pull from db?
+    with open("datasets/champions.json", "r") as file:
+        data_file = json.load(file)
+
+    y_output = ai.run_rf(role_rf, data, "role")
+    #gives champId
+    
+    print(y_output) #for testing purposes
+    
+    champName = None
+    if y_output != None:
+        for i in data_file:
+            if i["id"] == y_output:
+                champName = i
+
+    #returns none if output is invalid
+    return champName
 
 def get_knn_output(knn_model, data):
-    y_output, y_data = ai.run_knn(knn_model, data)
+    y_output, _ = ai.run_knn(knn_model, data)
 
     #y_output is a list on np arrays right now
     y_list = []
@@ -61,3 +120,6 @@ def get_knn_output(knn_model, data):
         y_list.append(list(i))
 
     return y_list
+
+
+#TESTING
