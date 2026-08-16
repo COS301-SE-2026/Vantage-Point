762f2134a72cc9d7ee3ee88cc5662daa2c2f0d8e
OPhelia2024 <greyling.ophelia@tuks.co.za>
2026-08-16 08:22:17 +0000

    Attempt unit tests for CI/CD

--- backend/app/pred_engine/AI_models.py
+++ backend/app/pred_engine/AI_models.py
@@ -25,17 +25,17 @@ def avg_and_std(y_data):
 
 def create_rf_models():
     champ_rf = rf.final_train(
-        "/backend/app/pred_engine/Training_csv/champ_rf_training.csv",
+        "/app/pred_engine/Training_csv/champ_rf_training.csv",
         "champion",
     )
     item_rf = rf.final_train(
-        "/backend/app/pred_engine/Training_csv/item_rf_training.csv", "item"
+        "/app/pred_engine/Training_csv/item_rf_training.csv", "item"
     )
     role_rf = rf.final_train(
-        "/backend/app/pred_engine/Training_csv/role_rf_training.csv", "role"
+        "/app/pred_engine/Training_csv/role_rf_training.csv", "role"
     )
     skill_rf = rf.final_train(
-        "/backend/app/pred_engine/Training_csv/skill_rf_training.csv",
+        "/app/pred_engine/Training_csv/skill_rf_training.csv",
         "skill",
     )
 
@@ -44,7 +44,7 @@ def create_rf_models():
 
 def create_knn_model():
 
-    knn_models = knn.get_knn("/backend/app/pred_engine/Training_csv/knn_training.csv")
+    knn_models = knn.get_knn("/app/pred_engine/Training_csv/knn_training.csv")
 
     return knn_models
 
