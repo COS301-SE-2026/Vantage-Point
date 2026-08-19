fdf4103c465a331f7d3e29c95f05c9429001a9a4
Bit-Watcher <shaunmarx05@gmail.com>
2026-08-19 22:52:47 +0200

    Making changes and trying to fix errors

--- backend/app/api/router/ai_routes.py
+++ backend/app/api/router/ai_routes.py
@@ -1,49 +1,65 @@
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, HTTPException, FastAPI
 from app.Models.auth_model import User
 from app.api.auth import require_group
 from typing import Any, Annotated
 from app.database.session import get_session
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from backend.app.Models.riot_schemas import (MapReplay, MapSuggestData, MatchData, ItemData, RoleData)
-from app.pred_engine.ai_caller import (get_knn_output, get_champ_pred, get_item_pred, get_role_pred, get_skill_pred)
+from app.Models.riot_schemas import (MapReplay, MapSuggestData, MatchData, ItemData, RoleData)
+from app.pred_engine.ai_caller import (get_knn_output, get_champ_pred, get_item_pred, get_role_pred, get_skill_pred, create_models)
 from app.services.analytics import LiveAnalyticsServiceDep
 
 router = APIRouter(tags=["ai"])
 
 
-router.get(
-    "knn-model",
-)
-async def get_knn_output_model(_: Annotated[User, Depends(require_group(10))], match_id: str, puuid: str, session: Annotated[AsyncSession, get_session()], live_analytics: LiveAnalyticsServiceDep) -> Any:
-    data: Any = await live_analytics.map_suggest_data(match_id=match_id, puuid=puuid, session=session)
-    return await get_knn_output(data)
 
-router.get(
-    "champ-model",
+@router.get(
+    "/knn-model",
 )
-async def get_champ_pred_model(_: Annotated[User, Depends(require_group(10))], match_id: str, puuid: str, session: Annotated[AsyncSession, get_session()], live_analytics: LiveAnalyticsServiceDep) -> Any:
-    data: Any = await live_analytics.champion_data(match_id=match_id, puuid=puuid)
-    return await get_champ_pred(data)
+async def get_knn_output_model(_: Annotated[User, Depends(require_group(10))], match_id: str, puuid: str, session: Annotated[AsyncSession, Depends(get_session)], live_analytics: LiveAnalyticsServiceDep) -> Any:
+    try:
+        data: Any = await live_analytics.map_suggest_data(match_id=match_id, puuid=puuid, session=session)
+        return get_knn_output(data)
+    except HTTPException as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
-router.get(
-    "item-model",
+@router.get(
+    "/champ-model",
 )
-async def get_item_pred_model(_: Annotated[User, Depends(require_group(10))], match_id: str, puuid: str, session: Annotated[AsyncSession, get_session()], live_analytics: LiveAnalyticsServiceDep) -> Any:
-    data: Any = await live_analytics.item_data(match_id=match_id, puuid=puuid, session=session)
-    return await get_item_pred(data)
+async def get_champ_pred_model(_: Annotated[User, Depends(require_group(10))], match_id: str, puuid: str, session: Annotated[AsyncSession, Depends(get_session)], live_analytics: LiveAnalyticsServiceDep) -> Any:
+    try:
+        data: Any = await live_analytics.champion_data(match_id=match_id, puuid=puuid)
+        return get_champ_pred(data)
+    except HTTPException as e:
+            raise HTTPException(status_code=500, detail=str(e))
 
-router.get(
-    "role-model",
+@router.get(
+    "/item-model",
 )
-async def get_role_pred_model(_: Annotated[User, Depends(require_group(10))], match_id: str, puuid: str, session: Annotated[AsyncSession, get_session()], live_analytics: LiveAnalyticsServiceDep) -> Any:
-    data: Any = await live_analytics.role_data(match_id=match_id, puuid=puuid)
-    return await get_role_pred(data)
+async def get_item_pred_model(_: Annotated[User, Depends(require_group(10))], match_id: str, puuid: str, session: Annotated[AsyncSession, Depends(get_session)], live_analytics: LiveAnalyticsServiceDep) -> Any:
+    try:
+        data: Any = await live_analytics.item_data(match_id=match_id, puuid=puuid, session=session)
+        return get_item_pred(data)
+    except HTTPException as e:
+            raise HTTPException(status_code=500, detail=str(e))
 
-
-router.get(
-    "skill-model",
+@router.get(
+    "/role-model",
 )
-async def get_skill_pred_model(_: Annotated[User, Depends(require_group(10))], match_id: str, puuid: str, session: Annotated[AsyncSession, get_session()], live_analytics: LiveAnalyticsServiceDep) -> Any:
-    data: Any = await live_analytics.skill_data(match_id=match_id, puuid=puuid, session=session)
-    return await get_skill_pred(data)
+async def get_role_pred_model(_: Annotated[User, Depends(require_group(10))], match_id: str, puuid: str, session: Annotated[AsyncSession, Depends(get_session)], live_analytics: LiveAnalyticsServiceDep) -> Any:
+    try: 
+        data: Any = await live_analytics.role_data(match_id=match_id, puuid=puuid)
+        return get_role_pred(data)
+    except HTTPException as e:
+            raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get(
+    "/skill-model",
+)
+async def get_skill_pred_model(_: Annotated[User, Depends(require_group(10))], match_id: str, puuid: str, session: Annotated[AsyncSession, Depends(get_session)], live_analytics: LiveAnalyticsServiceDep) -> Any:
+    try:
+        data: Any = await live_analytics.skill_data(match_id=match_id, puuid=puuid, session=session)
+        return get_skill_pred(data)
+    except HTTPException as e:
+            raise HTTPException(status_code=500, detail=str(e))
