4c2b6fba680e0546bc8d30e2f87c0fcea90b1d17
Bit-Watcher <shaunmarx05@gmail.com>
2026-08-19 22:24:50 +0200

    Start to implement ai router

---
+++ backend/app/api/router/ai_routes.py
@@ -0,0 +1,20 @@
+from fastapi import Depends, APIRouter
+from app.Models.auth_model import User
+from app.api.auth import require_group
+from typing import Any, Annotated
+from app.database.session import get_session
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from backend.app.Models.riot_schemas import (MapReplay, MapSuggestData, MatchData, ItemData, RoleData)
+from app.pred_engine.ai_caller import (get_knn_output, get_champ_pred, get_item_pred, get_role_pred, get_skill_pred)
+from app.services.analytics import LiveAnalyticsServiceDep
+
+router = APIRouter(tags=["ai"])
+
+
+router.get(
+    "knn-model",
+)
+async def get_knn_model(match_id: str, puuid: str, session: Annotated[AsyncSession, get_session()], live_analytics: LiveAnalyticsServiceDep) -> Any:
+    data: Any = await live_analytics.map_suggest_data(match_id=match_id, puuid=puuid, session=session)
+    return await get_knn_output(data)
